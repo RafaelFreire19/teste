580f90a2a153605a54b134690f24f5ab3d94a85b
RafaelFreire19 <rafaelfreire.mat@gmail.com>
2020-06-17 11:06:25 -0300

    mensagem

--- Declaration/src/FromMyLove.java
+++ Declaration/src/FromMyLove.java
@@ -6,10 +6,6 @@ public class FromMyLove {
         System.out.print("How many times u want to hear, i love u? ");
         int n=input.nextInt();
 
-        for(int i=1;i<=n;i++){
-            System.out.println("I love u, my princess");
-        }
-
         input.close();
     }
 }
