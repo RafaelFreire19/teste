60d8755e748f64be06d79e300fe96c40a1cb60af
RafaelFreire19 <rafaelfreire.mat@gmail.com>
2020-06-17 17:08:01 -0300

    New programm

--- Declaration/src/FromMyLove.java
+++ Declaration/src/FromMyLove.java
@@ -5,6 +5,11 @@ public class FromMyLove {
         Scanner input= new Scanner(System.in);
         System.out.print("How many times u want to hear, i love u? ");
         int n=input.nextInt();
+        
+        for (int i=1;i<=n;i++){
+            System.out.println("I love u, my princess");
+
+        }
 
         input.close();
     }
