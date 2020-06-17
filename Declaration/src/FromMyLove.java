7c955bd98d9a366d9d604a91bf9c8dfa56720f23
RafaelFreire19 <rafaelfreire.mat@gmail.com>
2020-06-17 10:31:56 -0300

    Teste1

--- Declaration/src/FromMyLove.java
+++ Declaration/src/FromMyLove.java
@@ -1,17 +1,14 @@
 import java.util.Scanner;
 
-public class Exercicio10 {
+public class FromMyLove {
     public static void main(String[] args) {
         Scanner input= new Scanner(System.in);
-        
+        System.out.print("How many times u want to hear, i love u? ");
         int n=input.nextInt();
-        
-        for(int i=1;i<=n;i++){
-            if(n%i==0){
-                System.out.println(i);
-            }
-        }
 
+        for(int i=1;i<=n;i++){
+            System.out.println("I love u, my princess");
+        }
 
         input.close();
     }
--- Hello world/src/App.java
+++
@@ -1,9 +0,0 @@
-public class App {
-    /**
-     * So assistir os videos da Loiane Groner.
-     * @param argumentos
-     */
-    public static void main(String[] argumentos) {
-        System.out.println("Ola mundo!");
-    }
-}
