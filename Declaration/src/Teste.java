0bb293e18559b14ab41dbb8babd6cf1b6d36b3e2
RafaelFreire19 <rafaelfreire.mat@gmail.com>
2020-06-18 11:54:27 -0300

    mensagem atualizada

--- Declaration/src/Teste.java
+++ Declaration/src/Teste.java
@@ -1,6 +1,17 @@
+import java.util.Scanner;
+
 public class Teste {
     public static void main(String[] args) {
-        System.out.println("ola");
-        System.out.println("Mundo");
+      Scanner input= new Scanner(System.in);
+
+        System.out.print("How many times do u want see the message?");
+        int n =input.nextInt();
+
+        for(int i=0;i<n;i++){
+            System.out.println("LOVE U, CALM DOWN, MY LOVE");
+        }
+
+
+      input.close();
     }
 }
