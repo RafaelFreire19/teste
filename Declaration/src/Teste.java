a5a746683502c4fe1b4a02b4495eed091b19a471
RafaelFreire19 <rafaelfreire.mat@gmail.com>
2020-06-20 00:25:24 -0300

    new message

--- Declaration/src/Teste.java
+++ Declaration/src/Teste.java
@@ -1,17 +1,7 @@
-import java.util.Scanner;
-
 public class Teste {
     public static void main(String[] args) {
-      Scanner input= new Scanner(System.in);
+      
+        System.out.print("Feliz dia 19, te amo.");
 
-        System.out.print("How many times do u want see the message?");
-        int n =input.nextInt();
-
-        for(int i=0;i<n;i++){
-            System.out.println("LOVE U, CALM DOWN, MY LOVE");
-        }
-
-
-      input.close();
     }
 }
